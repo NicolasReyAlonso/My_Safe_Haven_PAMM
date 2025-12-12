5e130df93ed86ddf5b04ee48ae921ae914114b49
NicolasReyAlonso <nicolasreyalonso@gmail.com>
2025-12-12 11:39:47 +0000

    add subscriptions table to db

--- Backend/db/init.sql
+++ Backend/db/init.sql
@@ -30,3 +30,11 @@ CREATE TABLE IF NOT EXISTS chat_messages (
     content TEXT NOT NULL,
     date TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+CREATE TABLE IF NOT EXISTS subscriptions (
+    subscription_id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    haven_id INTEGER NOT NULL REFERENCES havens(haven_id) ON DELETE CASCADE,
+    subscribed_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    UNIQUE (user_id, haven_id)
+);
